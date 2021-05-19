5551f57b79de5211e9d38c413e9a600eee89d1a3
Daniel Minson Pucciariello <daniel.minsonp@gmail.com>
2021-05-19 15:57:26 -0300

    ref e emergencia

--- aps5_vminson.cpp
+++ aps5_vminson.cpp
@@ -6,33 +6,33 @@ BusOut motor_y(PB_2,PB_11,PB_12,PA_11);
 
 BusOut motor_z(PA_12,PC_5,PC_6,PC_8);
 //Botao de emergencia
-DigitalIn botao_emergencia(PC_13);
+InterruptIn botao_emergencia(PC_13);
 //Endstops
-DigitalIn endstop_x(PC_15);
-DigitalIn endstop_y(PA_15);
-DigitalIn endstop_z(PB_15);
+InterruptIn endstop_x(PC_15);
+InterruptIn endstop_y(PB_15);
+InterruptIn endstop_z(PA_15);
 //DEFINIR PORTAS PRA ESSES TB
 
 //botoes para controle da movimentação em Z
 AnalogIn botoes_nucleo(A0);
 
 //passos dos fusos
-passo_linear_x=3;//avanço linear do eixo X
-passo_linear_y=3;//avanço linear do eixo Y
-passo_linear_z=10;//avanço linear do eixo Z
+int passo_linear_x=3;//avanço linear do eixo X
+int passo_linear_y=3;//avanço linear do eixo Y
+int passo_linear_z=10;//avanço linear do eixo Z
 
 //passos por revolução dos motores 
-steps_por_revol_x=512;
-steps_por_revol_y=512;
-steps_por_revol_z=512;
+int steps_por_revol_x=512;
+int steps_por_revol_y=512;
+int steps_por_revol_z=512;
 
 //Declaração das portas do Joystick (x e y)
 AnalogIn Ax (PC_3);
 AnalogIn Ay (PC_2);
 //declaração das portas dos leds
 DigitalOut led_vermelho(PD_2);
-DigitalOut led_verde(PD_2);
-DigitalOut led_amarelo(PD_2);
+DigitalOut led_verde(PC_11);
+DigitalOut led_amarelo(PC_10);
 DigitalOut led_azul(PC_12);
 
 // Declaracao variavel de posicionamento do joystick
@@ -44,7 +44,7 @@ int ref_y_feito=0;
 int ref_z_feito=0;
 
 //variável responsável por determinar se o sistema está em estado de emergência ou não. Estado igual a 1 significa que o sistema não está em estado de emergência
-int estado=1;
+int estado_sis=1;
 
 //ESSA PARTE VAI TER QUE REFAZER e ALTERAR OS VALORES
 // X Joystick
@@ -107,23 +107,45 @@ void motor_z_sentido_2(float tempo){
     }
 
 void Mx_off(){
-    motor_x=(0,0,0,0)
+    motor_x=0,0,0,0;
     }
 void My_off(){
-    motor_y=(0,0,0,0)
+    motor_y=0,0,0,0;
     } 
 void Mz_off(){
-    motor_z=(0,0,0,0)
+    motor_z=0,0,0,0;
     } 
 
-void emer(){
-    estado=0;
+void Mx_ref(){
+    motor_x=0,0,0,0;//desliga o motor 
+    ref_x_feito=1;//define que o referenciamento foi concluido
+    }
+void My_ref(){
+    motor_y=0,0,0,0;
+    ref_y_feito=1;
+    } 
+void Mz_ref(){
+    motor_z=0,0,0,0;
+    ref_z_feito=1;
+    } 
+
+void be(){
+    estado_sis=0;
     ref_x_feito=0;
     ref_y_feito=0;
     ref_z_feito=0;
 
-    //entrada no estado de emergencia e perdendo o referenciamento
+    //entrada no estado de emergencia e perdendo o referenciamento com botao de emergencia
     }
+void sair_emer(){
+    estado_sis=1;
+}
+void endstop_crash(){
+    estado_sis=0;
+    ref_x_feito=0;
+    ref_y_feito=0;
+    ref_z_feito=0;
+}
 int main(){
     Mz_off();
     Mx_off();
@@ -135,12 +157,59 @@ int main(){
     int vy_inv=vy;
     int vz=0.01;
     int vz_inv=vz;
+    botao_emergencia.mode(PullUp);
  while(1){
-        botao_emergencia.rise(&emer);
-        
+        botao_emergencia.fall(&be);
         x = Ax.read_u16();//ou Ax.read*1000()
         y = Ay.read_u16();//ou Ay.read*1000()   
+        if(estado_sis==1){//não está em estado emergencia
+            //HOMING
+        if(ref_x_feito==0 && ref_y_feito==0 && ref_z_feito==0){
+        motor_x_sentido_1(vx);
+        motor_y_sentido_1(vy);
+        motor_z_sentido_1(vz);
+        endstop_z.fall(&Mz_ref);//isso daqui vai funcionar um por vez?
+        endstop_y.fall(&My_ref);
+        endstop_x.fall(&Mx_ref);
+        }
         
+        else if(ref_x_feito==0 && ref_y_feito==0){
+            motor_y_sentido_1(vy);
+            motor_x_sentido_1(vx);
+            endstop_y.fall(&My_ref);
+            endstop_x.fall(&Mx_ref);
+        }
+
+        else if(ref_x_feito==0 && ref_z_feito==0){
+            motor_x_sentido_1(vx);
+            motor_z_sentido_1(vz);
+            endstop_x.fall(&Mx_ref);
+            endstop_z.fall(&Mz_ref);
+        }
+        else if(ref_y_feito==0 && ref_z_feito==0){
+            motor_y_sentido_1(vy);
+            motor_z_sentido_1(vz);
+            endstop_y.fall(&My_ref);
+            endstop_z.fall(&Mz_ref);
+        }
+        else if(ref_y_feito==0){
+            motor_y_sentido_1(vy);
+            endstop_y.fall(&My_ref);
+        }
+        else if(ref_x_feito==0){
+            motor_x_sentido_1(vx);
+            endstop_x.fall(&Mx_ref);
+        }
+        else if(ref_z_feito==0){
+            motor_z_sentido_1(1);
+            endstop_z.fall(&Mz_ref);
+        }
+        
+        else{
+            endstop_x.fall(&endstop_crash);//interrupção devido à colisão de um endstop
+            endstop_y.fall(&endstop_crash);//interrupção devido à colisão de um endstop
+            endstop_z.fall(&endstop_crash);//interrupção devido à colisão de um endstop
+
         if(x > CXmax){
            // int vx = map(x, CXmax, Xmax, 5, 0.5);
             motor_x_sentido_1(vx); //dando certo
@@ -157,26 +226,33 @@ int main(){
            // int vy_inv = map(y, CYmin ,Ymin, 0.5, 5);
             motor_y_sentido_2(vy_inv);        
         }
-        if(mov_z1.read() == 0){
+        // if(mov_z1.read() == 0){
+        //     motor_z_sentido_1(vz);
+        // }
+        // if(mov_z2.read() == 0){
+        //     motor_z_sentido_2(vz);
+        // }
+        // else if(mov_z1.read()=1 ||mov_z2.read()=1){
+        //     Mz_off();
+        // }
+     
+        if(botoes_nucleo>0.1000 && botoes_nucleo<0.1020){
             motor_z_sentido_1(vz);
         }
-        if(mov_z2.read() == 0){
-            motor_z_sentido_2(vz);
-        }
-        else if(mov_z1.read()=1 ||mov_z2.read()=1){
-            Mz_off();
-        }
-     
-    //endstops
-        // if(botao_y.read()==0){
-            
-        // }
-        // if(botao_x.read()==0){
-        //     motor_x_sentido_1();
-        // }
 
-        // if(botao_z.read()==0){
-        //     motor_z_sentido_1();
-        // }      
+        if(botoes_nucleo>0.25 && botoes_nucleo<0.26){
+            motor_z_sentido_2(vz_inv);
+        }
+
+        // if(botoes_nucleo>0.800 && botoes_nucleo<0.815){
+        //    // enter
+        // }
+        }
+        }
+        else{
+            be();
+            botao_emergencia.rise(&sair_emer);
+        }
+ }
+
     }
-}
