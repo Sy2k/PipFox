4c242aec6480954cc9495ad9c7eda5c47cf679ac
Stephanie Liu <49533602+Sy2k@users.noreply.github.com>
2021-05-26 11:38:26 -0300

    algumas coisas adicionadas, ainda nao mexam nesses arquivos

--- Organizando/funcao.cpp
+++ Organizando/funcao.cpp
@@ -1,71 +1,48 @@
 // --------------------------- Funcoes ------------------------------------------
+// -------------- Importando o arquivo de setup --------------
+#include "setup.h"
 
+// --------------------- Display ---------------------------
 void print_lcd(int step_x, int step_y, int step_z)
 {
-        lcd.cls();//limpar a tela
-    //  float distancia_x = step_x*passo_linear_x/steps_por_revol_x;//calculo da distancia percorrida em x
-    //  float distancia_y = step_y*passo_linear_y/steps_por_revol_y;//calculo da distancia percorrida em y
-    //  float distancia_z = step_z*passo_linear_z/steps_por_revol_z;//calculo da distancia percorrida em z
-    //  lcd.printf("dx=%.0f dy=%.0f\ndz=%.0f", distancia_x, distancia_y, distancia_z);
-    //  wait(1);//COLOCAR NA MESMA POSICAO
-        lcd.cls();
-        lcd.printf("Px=%2d Py=%2d\nPz=%2d", step_x, step_y, step_z);
-    //  wait(1);
+        tft.setTextColor(GREEN);
+        tft.setTextSize(3);
+        tft.setCursor(0,10); //  Orientação do texto X,Y
+        int distancia_x = step_x*passo_linear_x/steps_por_revol_x;//calculo da distancia percorrida em x
+        int distancia_y = step_y*passo_linear_y/steps_por_revol_y;//calculo da distancia percorrida em y
+        int distancia_z = step_z*passo_linear_z/steps_por_revol_z;//calculo da distancia percorrida em z
+        tft.printf("distancia_x=%.2f\ndistancia_y=%.2f\ndistancia_z=%.f\npassos_x=%.0f\npassos_y=%.0f", distancia_x, distancia_y, distancia_z,step_x,step_y);
+        tft.printf("\npassos_z=%.0f",step_z);
 }
 
-void motor_x_sentido_1(int tempo){
-    for(int i = 0; i <4;i++){
-        motor_x = 1 << i;       //verificar se o estado do sistema eh 0 ou 1
-        wait_ms(tempo);
+// --------------------- Motores ---------------------------
+void motor(int tempo, int sentido,int motor){
+    if(sentido==0){
+        for(int i = 0; i <4;i++){
+            motor = 1 << i;       //verificar se o estado do sistema eh 0 ou 1
+            wait_ms(tempo);
+        }
     }
-}
-
-void motor_x_sentido_2(int tempo){
-    for(int i= 3;i>-1;i--){
-        motor_x = 1 << i;
-        wait_ms(tempo);
-    }
-}
-    
-void motor_y_sentido_1(int tempo){
-    for(int i = 0; i <4;i++){
-        motor_y = 1 << i;
-        wait_ms(tempo);
-    }
-}
-
-
-void motor_y_sentido_2(int tempo){
-    for(int i= 3;i>-1;i--){
-        motor_y = 1 << i;
-        wait_ms(tempo);
-    }
-}
-    
-void motor_z_sentido_1(int tempo){
-    for(int z = 0; z <4;z++){
-        motor_z = 1 << z;
-        wait_ms(tempo);
-    }
-}
-
-void motor_z_sentido_2(int tempo){
-    for(int z = 3; z > -1;z--){
-        motor_z = 1 << z;
-        wait_ms(tempo);
+    else if(sentido==1){
+        for(int i= 3;i>-1;i--){
+            motor = 1 << i;
+            wait_ms(tempo);
+        }
     }
 }
 
 void Mx_off(){
-    motor_x=0,0,0,0;//mao desligar o motor - somente cortar a alimentacao 
+    motor_x = 0,0,0,0; 
 }
+
 void My_off(){
-    motor_y=0,0,0,0;
+    motor_y = 0,0,0,0;
 } 
 void Mz_off(){
-    motor_z=0,0,0,0;
+    motor_z = 0,0,0,0;
 } 
 
+// --------------------- ref ---------------------------
 void ref(){
     if (movendo_em_x==1)
     {
@@ -115,12 +92,12 @@ void be(){
     print_valor_pos=0;
     char solta[3]={0,0,0};
     char coleta[3]={0,0,0};
-    
     debounce_emer.start();//iniciar timer para debounce
     pc.printf("\r estado de emergencia\n");
     // lcd.printf("\r estado de emergencia\n");
     //entrada no estado de emergencia e perdendo o referenciamento com botao de emergencia
 }
+
 void sair_emer(){
     if(debounce_emer.read_ms()>15){//CHECAR
         estado_sis=1;
@@ -132,10 +109,3 @@ void sair_emer(){
         // lcd.printf("\r saindo do estado de emergência\n");
     }
 }
-// void endstop_crash(){
-//     estado_sis=0;
-//     ref_x_feito=0;
-//     ref_y_feito=0;
-//     ref_z_feito=0;
-//     pc.printf("\r estado de emergencia devido a batida \n");
-// }
--- Organizando/setup.h
+++ Organizando/setup.h
@@ -1,103 +1,102 @@
 // --------------------------- SETUP ------------------------------------------
 #ifndef FUNCAO
 #define FUNCAO
-#include "TextLCD.h"
-#include "mbed.h"//Definição das portas dos motores
+//----------------------- incluindo bibliotecas -----------------------
+#include "mbed.h"
+#include <MCUFRIEND_kbv.h>
+#include "Arduino.h"
+
+//----------------------- Configuracao do display -----------------------
+MCUFRIEND_kbv tft;
+uint8_t Orientation = 1;  
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#define Xmax 65000 // X Joystick
+#define Xmin 0 // X Joystick
+#define CXmin 31000 // Centro Joystick X
+#define CXmax 33500 // Centro Joystick X
+#define CYmin 31000 // Centro Joystick Y
+#define CYmax 34500 // Centro Joystick Y
+#define Ymax 65000 // Y Joystick
+#define Ymin 0 // Y Joystick
+
+//----------------------- Monitor serial -----------------------
+Serial pc(USBTX,USBRX);
+
+//----------------------- Definição das portas dos motores -----------------------
 BusOut motor_x(PC_4,PB_13,PB_14,PB_1);
-
 BusOut motor_y(PB_2,PB_11,PB_12,PA_11);
-
 BusOut motor_z(PA_12,PC_5,PC_6,PC_8);
 
-//LCD
-TextLCD lcd(D8, D9, D4, D5, D6, D7); //definição das portas rs,e,d0,d1,d2,d3
+//----------------------- Botoes -----------------------
+InterruptIn botao_emergencia(PC_13); // BE
+InterruptIn endstops(PA_15); // Endstop
+DigitalIn enter(PB_15); // Enter
+DigitalIn z1(PA_13); // movimentacao emZ+
+DigitalIn z2(PC_15); // movimentacao em Z- 
 
-//inicializar usb para visualizar dados em monitor serial
-Serial pc(USBTX,USBRX);
-//debounce para o botao de emergencia
-Timer debounce_emer;
-
-Timer debounce_endstop;
-
-Timer tempo_de_funcionamento;
-
-//Botao de emergencia
-InterruptIn botao_emergencia(PC_13);
-
-//Endstops - interrupções
-InterruptIn endstops(PA_15); //ou pc14
-DigitalIn enter(PB_15);
-
-//botoes para controle da movimentação em Z
-AnalogIn botoes_nucleo(A0);
-
-//passos dos fusos
-int passo_linear_x = 3;//avanço linear do eixo X
-int passo_linear_y = 3;//avanço linear do eixo Y
-int passo_linear_z = 10;//avanço linear do eixo Z
-
-//passos por revolução dos motores 
-int steps_por_revol_x = 512;
-int steps_por_revol_y = 512;
-int steps_por_revol_z = 512;
-
-//Declaração das portas do Joystick (x e y)
+//----------------------- Declaração das portas do Joystick (x e y) -----------------------
 AnalogIn Ax (PC_3);
 AnalogIn Ay (PC_2);
 
-//declaração das portas dos leds
+//----------------------- Declaração das portas dos leds -----------------------
 DigitalOut led_vermelho(PD_2);
 DigitalOut led_verde(PC_11);
 DigitalOut led_amarelo(PC_10);
 DigitalOut led_azul(PC_12);
 
-//arrays
+//----------------------- debounce para o botao de emergencia -----------------------
+Timer debounce_emer;
+Timer debounce_endstop;
+Timer tempo_de_funcionamento;
+
+//----------------------- arrays -----------------------
 char solta[3];
 char coleta[3];
 char atual[3];
 char distancia_coleta_atual[3];
 char distancia_solta_coleta[3];
 
-// Declaracao variavel de posicionamento do joystick
-int x, y;
+// ----------------------- passos dos fusos e motor-----------------------
+int passo_x = 3; // avanço linear do eixo X
+int passo_y = 3; // avanço linear do eixo Y
+int passo_z = 10; // avanço linear do eixo Z
+int steps_rev_x = 512; //passos por revolução dos motores 
+int steps_rev_y = 512; //passos por revolução dos motores 
+int steps_rev_z = 512; //passos por revolução dos motores 
+
+//----------------------- sistema está em estado de emergência ou não -----------------------
+int estado_sis = 1; //0 -> emergencia; -> 1 funcionamento normal; 2-> fim do processo
+
+// ----------------------- contadores de passos -----------------------
+int step_x = 0;
+int step_y = 0;
+int step_z = 0;
+
+// ----------------------- variaveis relacionadas ao motor e o joystick -----------------------
+int x, y, vx, vx_inv, vy, vy_inv, vz, vz_inv;
 bool movendo_em_x,movendo_em_y,movendo_em_z;
 
-//variáveis que mostram em que direcao a movimentacao esta sendo feita
-
-
-//variáveis que demonstram se o sistema está referenciado ou não em cada um dos eixos. 
+//----------------------- variáveis de referenciamento -----------------------
 bool ref_x_feito = 0;
 bool ref_y_feito = 0;
 bool ref_z_feito = 0;
 
-//variável responsável por determinar se o sistema está em estado de emergência ou não. Estado igual a 1 significa que o sistema não está em estado de emergência
-int estado_sis = 1;//0 -> emergencia; -> 1 funcionamento normal; 2-> fim do processo
-
-//inicialização dos contadores de passos
-int step_x = 0;
-int step_y = 0;
-int step_z = 0;
 //variaveis variaveis responsaveis pela logica
-bool determinar_ponto=1;//1 -> ponto de coleta; 0 -> ponto de solta
-bool print_valor_pos=1;
-bool tipo_de_movimento=0;//0 atual para coleta ou 1 de pega para solta
-bool rotina_principal=0;//1 para rotina principal e 0 para outras rotinas
-
-//ESSA PARTE VAI TER QUE REFAZER e ALTERAR OS VALORES
-// X Joystick
-#define Xmax 65000
-#define Xmin 0
-
-// Centro Joystick X
-#define CXmin 31000
-#define CXmax 33500
-
-// Centro Joystick Y
-#define CYmin 31000
-#define CYmax 34500
-
-// Y Joystick
-#define Ymax 65000
-#define Ymin 0
+bool determinar_ponto = 1; //1 -> ponto de coleta; 0 -> ponto de solta
+bool print_valor_pos = 1;
+bool tipo_de_movimento = 0; //0 atual para coleta ou 1 de pega para solta
+bool rotina_principal = 0; //1 para rotina principal e 0 para outras rotinas
 
 #endif 
+
+
+
